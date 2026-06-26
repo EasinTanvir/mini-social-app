f25c738a70caae379fbd481d2f4d01846c3664df
EasinTanvir <easintanvir102@gmail.com>
2026-06-26 22:42:43 +0600

    refactor

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,17 +4,13 @@ const helmet = require("helmet");
 const cors = require("cors");
 const compression = require("compression");
 
-const { FRONTEND_ORIGIN } = require("../config");
 const { invalidRoutes, errorMethod } = require("./utils/errorHandler");
 const authRoute = require("./routes/auth.route");
 const postRoute = require("./routes/post.route");
 
 const app = express();
-
 app.use(helmet());
-
 app.use(cors());
-
 app.use(compression());
 app.use(express.json());
 app.use(morgan("dev"));
